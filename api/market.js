92c5d63a31d78f1ccd4ed8f55c89d6ff754cf8b2
jskb8fhkjc-hash <jskb8fhkjc@privaterelay.appleid.com>
2026-05-27 23:36:13 +0200

    Update market.js

--- api/market.js
+++ api/market.js
@@ -1,5 +1,25 @@
 import { Redis } from "@upstash/redis";
 
+function uuidv4(){
+
+return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx"
+
+.replace(/[xy]/g,function(c){
+
+const r =
+Math.random()*16|0;
+
+const v =
+c==="x"
+? r
+: (r&0x3|0x8);
+
+return v.toString(16);
+
+});
+
+}
+
 /*
 ==================================================
 REDIS
@@ -388,7 +408,7 @@ API_KEY,
 USER_KEY,
 
 "x-request-id":
-Date.now().toString()
+uuidv4()
 
 }
 }
@@ -445,7 +465,7 @@ API_KEY,
 USER_KEY,
 
 "x-request-id":
-Date.now().toString()
+uuidv4()
 
 }
 }
